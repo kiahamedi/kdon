8541ba5daac267fd9ff0e535050c2d07ab9783af
Kia Hamedi <kia.arta9793@gmail.com>
2018-04-16 17:33:23 +0430

    upload script

---
+++ kdon.py
@@ -0,0 +1,21 @@
+import urllib2
+from os import system
+from bs4 import BeautifulSoup
+import wget
+
+system('figlet "kia hamedi"')
+
+url = raw_input("Enter the serial URL:")
+
+conn = urllib2.urlopen(url)
+html = conn.read()
+
+soup = BeautifulSoup(html,"html.parser")
+links = soup.find_all('a')
+
+for tag in links:
+    link = tag.get('href',None)
+    if link is not None:
+        print(url+str(link))
+
+        wget.download(url+str(link))
